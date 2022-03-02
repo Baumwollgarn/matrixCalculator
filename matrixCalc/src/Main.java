33380243fcc46647b45337237fe2554008fd4b13
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-03-02 10:18:48 +0100

    Refactored compareMatrix method

--- matrixCalc/src/Main.java
+++ matrixCalc/src/Main.java
@@ -32,16 +32,13 @@ public class Main {
 
         return matrix;
     }
+
     /* Sumar dues matrius = A + B = 1A+1B,2A+2B... nB+nB
     *  PER SUMAR! HAN DE SER DE LA MATEIXA tamany */
 
     public static boolean compareMatrix(int[][] matrix1, int[][] matrix2) {
-        int rows1 = matrix1.length;
-        int columns1 = matrix1[0].length;
-        int rows2 = matrix2.length;
-        int columns2 = matrix2[0].length;
 
-        return rows1 == rows2 && columns1 == columns2;
+        return matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length;
     }
 
 
@@ -59,21 +56,16 @@ public class Main {
 
     public static int[][] sumar(int[][] matrix1, int[][] matrix2){
 
-        if(!compareMatrix(matrix1,matrix2)) return matrix1;
 
         int rows = matrix1.length;
         int columns = matrix1[0].length;
 
-        for (int row = 0; row < rows; row++)
-            for (int col = 0; col < columns; col++)
-                matrix1[row][col] += matrix2[row][col];
-        /*
         if (compareMatrix(matrix1,matrix2)) {
             for (int row = 0; row < rows; row++)
                 for (int col = 0; col < columns; col++)
                     matrix1[row][col] += matrix2[row][col];
         }
-         */
+
         return matrix1;
     }
 
@@ -110,7 +102,12 @@ public class Main {
         int[][] matrix3 = new int[matrix1[0].length][matrix2.length];
 
         if (matrix1.length == matrix2[0].length) {
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix2[0].length; j++) {
+                    matrix3[j][i] = matrix1[j][i] * matrix2[j][i];
 
+                }
+            }
         }
         return matrix3;
     }
@@ -172,13 +169,13 @@ public class Main {
     *
     *
     * */
-    public static int determinant3x3(){
+    /* public static int determinant3x3(){
 
-    }
+    } */
 
     public static void main(String[] args) {
-        String clase = "Jodidos";
-        System.out.println(clase);
+        /* String clase = "Jodidos";
+        System.out.println(clase); */
 
         int [][] A = {{1,2,3},{4,3,1}, {1,0,1}};
         int [][] A2 = {{0,2,5},{2,3,1}, {4,1,2}};
@@ -186,9 +183,7 @@ public class Main {
         int [][] C = {{3}};
         int [][] D = {{1},{2},{3}};
 
-        System.out.println(Arrays.deepToString(sumar(A, A2)));
-
-        printMatrix(A);
+        printMatrix(sumar(A,A2));
         //Farem matrius de fins a 3x3--> Exemples: 2x2 3x2 3x1...2x1;
         /*
         - nomenclatura; matriu A B C .
